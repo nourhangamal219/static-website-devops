ab15a15b5d962f62d11f55c4a2fcfa6c52a34f65
nourhangamal219 <69842555+nourhangamal219@users.noreply.github.com>
2025-05-28 03:35:43 +0400

    Update variables.tf
    Add mail variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,12 +2,14 @@ variable "region" {
   default = "eu-west-2"
 }
 
-variable "bucket_name" {
-  description = "S3 bucket for static site"
-  type        = string
-}
-
 variable "domain_name" {
   description = "Custom domain name"
   type        = string
 }
+
+variable "mail" {
+  description = "E-mail to get websiet health notification"
+  type        = string
+}
+
+
