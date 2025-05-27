55383c042ea942ddf863a81e806db91ee0d3a159
nourhangamal219 <69842555+nourhangamal219@users.noreply.github.com>
2025-05-28 01:44:20 +0400

    Update main.tf
    Add CDN configurations

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,3 +50,57 @@ resource "aws_s3_bucket_policy" "public-content" {
   })
 }
 
+# set S3 as origin for CloudFront
+resource "aws_cloudfront_origin_access_control" "s3-origin" {
+  name                              = "static-website-origin"
+  description                       = "Origin for static website"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+# Create CloudFront for caching
+resource "aws_cloudfront_distribution" "site-cache" {
+  origin {
+    domain_name              = var.domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3-origin.id
+    origin_id                = "S3Origin"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    target_origin_id = "S3Origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "CDN-Static-Website"
+  }
+}
+
+
+
+  
