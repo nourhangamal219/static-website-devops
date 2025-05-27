435faf0048db3022e8f36b6ea6cdbce513193b64
nourhangamal219 <69842555+nourhangamal219@users.noreply.github.com>
2025-05-28 01:45:40 +0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,10 +82,6 @@ resource "aws_cloudfront_distribution" "site-cache" {
       }
     }
   }
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
 viewer_certificate {
     cloudfront_default_certificate = true
   }
