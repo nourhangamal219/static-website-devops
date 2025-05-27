cd30874f370ed594d87aff4eb4c8f23b5bd877f4
nourhangamal219 <69842555+nourhangamal219@users.noreply.github.com>
2025-05-28 01:53:24 +0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,6 @@ output "website_url" {
   value = aws_s3_bucket_website_configuration.static-website-conf.website_endpoint
 }
 
+output "cdn_domain" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
