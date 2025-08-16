bf67c51441281cdef2aec6d0343c6501bce4547a
nourhangamal219 <69842555+nourhangamal219@users.noreply.github.com>
2025-08-16 16:34:09 +0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tf-state-bucket"
+    bucket         = "noura-tf-state-bucket"
     key            = "devops-static-site/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
